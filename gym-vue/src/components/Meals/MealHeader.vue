9a0294697e56de503f856457f308cc90f02ab66f
Gina <roes1635000@gmail.com>
2026-02-04 22:03:43 +0800

    2026.02.04_上傳雲端備份

---
+++ gym-vue/src/components/Meals/MealHeader.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps({
+  title: String,
+  desc: String
+})
+</script>
+
+<template>
+  <div class="mb-4">
+    <h3 class="fw-bold">{{ title }}</h3>
+    <h6 class="text-muted">{{ desc }}</h6>
+  </div>
+</template>
---
+++ gym-vue/src/views/meals/MealOrderConfirm-View.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+
+
+<template>
+
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-6">
+      內容
+    </div>
+  </div>
+</div>
+
+</template>
+
+
+
+<style scoped>
+
+</style>
